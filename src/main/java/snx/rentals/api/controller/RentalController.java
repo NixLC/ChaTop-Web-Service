364d70f8e19672ae8b48605aa27557b9db159864
Nicolas LE CORFF <nicolas.lecorff@gmail.com>
2024-07-06 18:15:31 +0200

    remove method implementation because parent method is no longer abstract

--- src/main/java/snx/rentals/api/controller/RentalController.java
+++ src/main/java/snx/rentals/api/controller/RentalController.java
@@ -1,9 +1,7 @@
 package snx.rentals.api.controller;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import snx.rentals.api.model.dto.DTO;
 import snx.rentals.api.model.entity.Rental;
 import snx.rentals.api.repository.RentalRepository;
 
@@ -12,10 +10,5 @@ import snx.rentals.api.repository.RentalRepository;
 public class RentalController extends GenericController<Rental>{
     public RentalController(RentalRepository repository) {
         super(repository);
-    }
-
-    @Override
-    public ResponseEntity<DTO<Rental>> get(Integer id) {
-        return null;
     }
 }
--- src/main/java/snx/rentals/api/controller/UserController.java
+++ src/main/java/snx/rentals/api/controller/UserController.java
@@ -1,9 +1,7 @@
 package snx.rentals.api.controller;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import snx.rentals.api.model.dto.DTO;
 import snx.rentals.api.model.entity.User;
 import snx.rentals.api.repository.UserRepository;
 
@@ -12,11 +10,5 @@ import snx.rentals.api.repository.UserRepository;
 public class UserController extends GenericController<User> {
     public UserController(UserRepository repository) {
         super(repository);
-    }
-
-    @Override
-    public ResponseEntity<DTO<User>> get(Integer id) {
-        User user = getOne(id).getBody();
-        return ResponseEntity.ok(user.toDTO());
     }
 }
