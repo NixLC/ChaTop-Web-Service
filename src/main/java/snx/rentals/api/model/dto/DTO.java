5709ee7d691efef2f01ba4888e6dd5ee12d84e16
Nicolas LE CORFF <nicolas.lecorff@gmail.com>
2024-07-20 00:43:40 +0200

    add helper methods

--- src/main/java/snx/rentals/api/model/dto/DTO.java
+++ src/main/java/snx/rentals/api/model/dto/DTO.java
@@ -1,5 +1,14 @@
 package snx.rentals.api.model.dto;
 
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
 public interface DTO<T> {
-  T toPartialEntity();
+  DateTimeFormatter DEFAULT_DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+  static String instantToString(Instant instant, DateTimeFormatter formatter) {
+    return formatter.withZone(ZoneId.systemDefault())
+                              .format(instant);
+  }
+  T toEntity();
 }
