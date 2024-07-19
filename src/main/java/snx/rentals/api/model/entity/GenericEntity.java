80e3e68c1c3eca2573aeeb77b798615beaec78bf
Nicolas LE CORFF <nicolas.lecorff@gmail.com>
2024-07-20 00:41:02 +0200

    Fix method name collision

--- src/main/java/snx/rentals/api/model/entity/GenericEntity.java
+++ src/main/java/snx/rentals/api/model/entity/GenericEntity.java
@@ -2,11 +2,11 @@ package snx.rentals.api.model.entity;
 
 import snx.rentals.api.model.dto.DTO;
 
+
 public interface GenericEntity<T> {
     Integer getId();
     void update(T entity);
-    T createNewInstance();
-    default String getName() {
+    default String getEntityName() {
         return this.getClass().getName();
     }
     DTO<T> toDTO();
