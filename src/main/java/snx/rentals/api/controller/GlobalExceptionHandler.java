e3a5f3ef743e67915676edd69974ffb28b394f71
Nicolas LE CORFF <nicolas.lecorff@gmail.com>
2024-07-31 22:05:10 +0200

    improve exception handling

--- src/main/java/snx/rentals/api/controller/GlobalExceptionHandler.java
+++ src/main/java/snx/rentals/api/controller/GlobalExceptionHandler.java
@@ -2,81 +2,132 @@ package snx.rentals.api.controller;
 
 
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Path;
+import org.springframework.beans.TypeMismatchException;
 import org.springframework.dao.DataAccessException;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.DataRetrievalFailureException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.multipart.support.MissingServletRequestPartException;
 
 import java.time.LocalDateTime;
 import java.util.*;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+  @ExceptionHandler
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @ResponseBody
+  public Map<String, Object> handleValidationException(MethodArgumentNotValidException ex) {
+    Map<String, Object> errors = new LinkedHashMap<>();
+    Map<String, String> fieldErrors = new LinkedHashMap<>();
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ResponseBody
-    public Map<String, Object> handleValidationException(MethodArgumentNotValidException ex) {
-        Map<String, Object> errors = new LinkedHashMap<>();
-        Map<String, String> fieldErrors = new LinkedHashMap<>();
+    ex.getBindingResult().getAllErrors().forEach((error) -> {
+      String fieldName = ((FieldError) error).getField();
+      String errorMessage = error.getDefaultMessage();
+      if(error.contains(TypeMismatchException.class)){
+        errorMessage = "'" + ((FieldError) error).getRejectedValue() + "' is an invalid type for field '" + fieldName +"'";
+      }
+      fieldErrors.put(fieldName, errorMessage);
+    });
 
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            fieldErrors.put(fieldName, errorMessage);
-        });
+    errors.put("timestamp", LocalDateTime.now());
+    errors.put("errors", fieldErrors);
 
-        errors.put("timestamp", LocalDateTime.now());
-        errors.put("status", HttpStatus.BAD_REQUEST.value());
-        errors.put("error", HttpStatus.BAD_REQUEST.getReasonPhrase());
-        errors.put("errors", fieldErrors);
+    return errors;
+  }
 
-        return errors;
+  @ExceptionHandler({EntityNotFoundException.class, DataRetrievalFailureException.class})
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  @ResponseBody
+  public Map<String, Object> handleEntityNotFoundException(Exception ex) {
+    return produceMessages(ex);
+  }
+
+  @ExceptionHandler
+  @ResponseBody
+  public ResponseEntity<Map<String, Object>> handleDataAccessException(DataAccessException ex) {
+    return switch (ex.getClass().getSimpleName()) {
+      case "DataIntegrityViolationException" -> ResponseEntity.status(HttpStatus.BAD_REQUEST).body(produceMessages(ex));
+      default -> ResponseEntity.status(HttpStatus.NO_CONTENT).body(produceMessages(ex));
+    };
+  }
+
+  @ExceptionHandler
+  @ResponseBody
+  public ResponseEntity<Map<String, Object>> handleHttpServerErrorException(HttpServerErrorException ex) {
+    return ResponseEntity.status(ex.getStatusCode()).body(produceMessages(ex));
+  }
+
+  @ExceptionHandler
+  @ResponseStatus(HttpStatus.UNAUTHORIZED)
+  @ResponseBody
+  public Map<String, Object> handleBadCredentialsException(BadCredentialsException ex) {
+    return produceMessages(ex);
+  }
+
+  // Handle all non-explicit exception that could happen
+  @ExceptionHandler(Exception.class)
+  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+  @ResponseBody
+  public Map<String, Object> handleGenericException(Exception ex) {
+    return produceMessages(ex);
+  }
+
+  @ExceptionHandler
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @ResponseBody
+  public Map<String, Object> handleConstraintViolationException(ConstraintViolationException ex) {
+    Map<String, Object> errors = new LinkedHashMap<>();
+    Map<String, List<String>> fieldErrors = new LinkedHashMap<>();
+    Set<ConstraintViolation<?>> violations = ex.getConstraintViolations();
+
+    for (ConstraintViolation<?> violation : violations) {
+      String propertyName = getLastNode(violation.getPropertyPath());
+      fieldErrors.putIfAbsent(propertyName, new LinkedList<>());
+      fieldErrors.get(propertyName).add(violation.getMessage());
     }
+    errors.put("timestamp", LocalDateTime.now());
+    errors.put("errors", fieldErrors);
+    return errors;
+  }
 
-    @ExceptionHandler({EntityNotFoundException.class, DataRetrievalFailureException.class})
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ResponseBody
-    public Map<String, List<String>> handleEntityNotFoundException(EntityNotFoundException ex) {
-        return produceMessages(ex);
+  @ExceptionHandler
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @ResponseBody
+  public Map<String, Object> handleMissingServletRequestPartException(MissingServletRequestPartException ex) {
+    return produceMessages(ex);
+  }
+
+  private static Map<String, Object> produceMessages(Throwable ex) {
+    Map<String, Object> error = new LinkedHashMap<>();
+    List<String> messages = new LinkedList<>();
+
+    while (ex != null && ex.getMessage() != null) {
+      messages.add(ex.getMessage());
+      ex = ex.getCause();
     }
+    error.put("timestamp", LocalDateTime.now());
+    error.put("errors", messages);
 
-    @ExceptionHandler(DataAccessException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ResponseBody
-    public Map<String, List<String>> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
-        return produceMessages(ex);
+    return error;
+  }
+
+  private String getLastNode(Path propertyPath) {
+    String propertyName = null;
+    for (Path.Node node : propertyPath) {
+      propertyName = node.getName();
     }
-
-
-    @ExceptionHandler(HttpServerErrorException.InternalServerError.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ResponseBody
-    public Map<String, List<String>> handleInternalServerErrorException(HttpServerErrorException ex) {
-        return produceMessages(ex);
-    }
-
-    @ExceptionHandler(BadCredentialsException.class)
-    @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    @ResponseBody
-    public Map<String, List<String>> handleBadCredentialsException(BadCredentialsException ex) {
-        return produceMessages(ex);
-    }
-
-    private static Map<String, List<String>> produceMessages(Throwable ex) {
-        List<String> messages = new LinkedList<>();
-        while (ex != null && ex.getMessage() != null) {
-            messages.add(ex.getMessage());
-            ex = ex.getCause();
-        }
-        return Collections.singletonMap("messages", messages);
-    }
+    return propertyName;
+  }
 }
