f4e9acd51949922e0ed93f29494f80c09e9f8baa
Nicolas LE CORFF <nicolas.lecorff@gmail.com>
2024-07-31 22:04:49 +0200

    add JsonView restrictions

--- src/main/java/snx/rentals/api/model/dto/UserDto.java
+++ src/main/java/snx/rentals/api/model/dto/UserDto.java
@@ -1,30 +1,47 @@
 package snx.rentals.api.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonView;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import snx.rentals.api.model.entity.User;
+import snx.rentals.api.model.view.DtoViews;
 
 @Data
-@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
 public class UserDto implements DTO<User> {
   @JsonProperty(access = JsonProperty.Access.READ_ONLY)
   @Digits(integer = 10, fraction = 0)
+  @JsonView(DtoViews.Read.class)
   private Integer id;
+
   @NotBlank
   @Email
+  @JsonView({DtoViews.Read.class, DtoViews.Write.class})
   private String email;
+
   @NotBlank
+  @JsonView({DtoViews.Read.class, DtoViews.Write.class})
   private String name;
+
   @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
   @NotBlank
+  @JsonView(DtoViews.Write.class)
   private String password;
+
   @JsonProperty(value = "created_at", access = JsonProperty.Access.READ_ONLY)
+  @JsonView(DtoViews.Read.class)
   private String createdAt;
+
   @JsonProperty(value = "updated_at", access = JsonProperty.Access.READ_ONLY)
+  @JsonView(DtoViews.Read.class)
   private String updatedAt;
 
   @Override
