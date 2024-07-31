dbabb91913b7cfafb9f8a072856b2e4605b78bde
Nicolas LE CORFF <nicolas.lecorff@gmail.com>
2024-07-31 22:00:14 +0200

    add JsonView restriction

--- src/main/java/snx/rentals/api/model/dto/JwtResponse.java
+++ src/main/java/snx/rentals/api/model/dto/JwtResponse.java
@@ -1,4 +1,8 @@
 package snx.rentals.api.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonView;
+import snx.rentals.api.model.view.DtoViews;
+
+@JsonView({DtoViews.Read.class})
 public record JwtResponse(String token, long expiresIn) {
 }
