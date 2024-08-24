893a1611959706dd9ccfc3db3144e6ddc38cab53
Nicolas LE CORFF <nicolas.lecorff@gmail.com>
2024-08-24 20:02:13 +0200

    rename interface to a more explicit name this validator checks for advertised content type provided by HTTP request

--- src/main/java/snx/rentals/api/model/validation/ValidContentType.java
+++ src/main/java/snx/rentals/api/model/validation/ValidContentType.java
@@ -10,7 +10,7 @@ import java.lang.annotation.Target;
 @Constraint(validatedBy = FileTypeValidator.class)
 @Target({ ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER, ElementType.ANNOTATION_TYPE })
 @Retention(RetentionPolicy.RUNTIME)
-public @interface ValidFileType {
+public @interface ValidContentType {
   String message() default "Invalid file type";
   Class<?>[] groups() default {};
   Class<? extends Payload>[] payload() default {};
